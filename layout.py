b73f215a5b8e8cd2b0c4531cf2390d44af3b7682
trevor jaskot <trevord186@gmail.com>
2023-03-10 13:45:25 -0600

    updating OOP

--- layout.py
+++ layout.py
@@ -1,83 +1,73 @@
 import PySimpleGUI
 
-def set_gui_text(title=None, sizing=None, justifys=None):
-    # Example: sg_obj.Text("OpenCV Demo", size=(60, 1), justification="center")
-    sg_obj = PySimpleGUI.Text()
-    if title is None:
-        title = "OpenCV Demo"
-    if sizing is None:
-        sizing = (60, 1)
-    if justifys is None:
-        justifys = "center"
-    sg_obj.Text(title, size=sizing, justification=justifys)
-    return sg_obj.Text
+
+def add_slider(radio_name, radio_key, slider_key, radio_type="Radio", radio_size=(10, 1), slider_default=128,
+               slider_size=(40, 15), slider_range=(0, 255), slider_tick=1, slider_orientation="h"):
+    """
+    Function call to add Radio button and Slider component to layout.
+
+    Args:
+        radio_name: Name of Radio PySimpleGUI, needs to be defined
+        radio_key: Key of Radio PySimpleGui, needs to be defined
+        slider_key: Key of Slider, needs to be defined
+        radio_type: Radio type can be defined, default "Radio"
+        radio_size: Radio size is changeable, default (10,1)
+        slider_default: Default Slider position of 128
+        slider_size: Default slider size of (40, 15)
+        slider_range: Default slider range of 0 to 255, written as (0, 255)
+        slider_tick: Default slider movements, default is 1
+        slider_orientation: Default slider orientation is horizontal, written as "h"
+
+    Default Slider configs:
+    PySimpleGUI.Slider(
+        (0, 255),
+        128,
+        1,
+        orientation="h",
+        size=(40, 15),
+        key=<Slider Key>,
+    )
+
+    Returns: slider_list with radio button and slider objects
+
+    """
+    slider_list = []  # Array needed for objects added to list
+    # Example: sg.Radio("threshold", "Radio", size=(10, 1), key="-THRESH-")
+    layout_radio = PySimpleGUI.Radio(radio_name, radio_type, radio_size, radio_key)
+    slider_list.append(layout_radio)
+    # We need to verify with Radio key if the slider will be Canny, then add two slider key objects instead of one
+    if radio_key == "-CANNY-":
+        for canny_slider_key in ["-CANNY SLIDER A-", "-CANNY SLIDER B-"]:
+            layout_slider_canny = PySimpleGUI.Slider(
+                slider_range,
+                slider_default,
+                slider_tick,
+                orientation=slider_orientation,
+                size=slider_size,
+                key=canny_slider_key
+            )
+            slider_list.append(layout_slider_canny)
+    else:
+        layout_slider = PySimpleGUI.Slider(
+            slider_range,
+            slider_default,
+            slider_tick,
+            orientation=slider_orientation,
+            size=slider_size,
+            key=slider_key
+        )
+        slider_list.append(layout_slider)
+    return slider_list
 
 
-#
-# [sg.Image(filename="", key="-IMAGE-")],
-#     #        [sg.Radio("None", "Radio", True, size=(10, 1))],
-#     [
-#         sg.Radio("threshold", "Radio", size=(10, 1), key="-THRESH-"),
-#         sg.Slider(
-#             (0, 255),
-#             128,
-#             1,
-#             orientation="h",
-#             size=(40, 15),
-#             key="-THRESH SLIDER-",
-#         ),
-#     ],
-#     [
-#         sg.Radio("canny", "Radio", size=(10, 1), key="-CANNY-"),
-#         sg.Slider(
-#             (0, 255),
-#             128,
-#             1,
-#             orientation="h",
-#             size=(20, 15),
-#             key="-CANNY SLIDER A-",
-#         ),
-#         sg.Slider(
-#             (0, 255),
-#             128,
-#             1,
-#             orientation="h",
-#             size=(20, 15),
-#             key="-CANNY SLIDER B-",
-#         ),
-#     ],
-#     [
-#         sg.Radio("blur", "Radio", size=(10, 1), key="-BLUR-"),
-#         sg.Slider(
-#             (1, 11),
-#             1,
-#             1,
-#             orientation="h",
-#             size=(40, 15),
-#             key="-BLUR SLIDER-",
-#         ),
-#     ],
-#     [
-#         sg.Radio("hue", "Radio", size=(10, 1), key="-HUE-"),
-#         sg.Slider(
-#             (0, 225),
-#             0,
-#             1,
-#             orientation="h",
-#             size=(40, 15),
-#             key="-HUE SLIDER-",
-#         ),
-#     ],
-#     [
-#         sg.Radio("enhance", "Radio", size=(10, 1), key="-ENHANCE-"),
-#         sg.Slider(
-#             (1, 255),
-#             128,
-#             1,
-#             orientation="h",
-#             size=(40, 15),
-#             key="-ENHANCE SLIDER-",
-#         ),
-#     ],
-#     [sg.Button("Exit", size=(10, 1))],
-# ]
+# def set_gui_text(title=None, sizing=None, justifys=None):
+#     # Example: sg_obj.Text("OpenCV Demo", size=(60, 1), justification="center")
+#     sg_obj = PySimpleGUI.Text()
+#     if title is None:
+#         title = "OpenCV Demo"
+#     if sizing is None:
+#         sizing = (60, 1)
+#     if justifys is None:
+#         justifys = "center"
+#     sg_obj.Text(title, size=sizing, justification=justifys)
+#     return sg_obj.Text
--- main2.py
+++ main2.py
@@ -1,7 +1,8 @@
 import os
 import cv2
-import time
+import copy
 import json
+import time
 import layout
 import PySimpleGUI
 
@@ -17,18 +18,21 @@ import PySimpleGUI
 # my_c.l = ['1', '2']
 # print(my_c.l)
 # print(my_c)
-
 # exit()
 
+
 class Layout:
+    layout_dict = dict
+
     def __init__(self):
-        self.text = PySimpleGUI.Text()
-        self.image = PySimpleGUI.Image()
-        self.thres_slider = list
-        self.canny_slider = list
-        self.blur_slider = list
-        self.hue_slider = list
-        self.enhance_slider = list
+        self.text = []  # Array for PySimpleGUI.Text()
+        self.image = []  # Array for PySimpleGUI.Image()
+        self.thres_slider = []  # Array, instead of list
+        self.canny_slider = []  # Array, instead of list
+        self.blur_slider = []  # Array, instead of list
+        self.hue_slider = []  # Array, instead of list
+        self.enhance_slider = []  # Array, instead of list
+        self.exit_button = []  # Array, instead of list
 
     def __repr__(self):
         return (f"Layout Inner Class Properties -\n"
@@ -42,10 +46,85 @@ class Layout:
                 )
 
 
+# Instantiate class object for PySimpleGUI updates and reusable code
 my_c = Layout()
-my_c.text.DisplayText = "OpenCV Demo"
-my_c.text.Size = (60, 1)
-my_c.text.Justification = "center"
+
+# Create separate dictionary items and then have those accessible to add/update
+layout_text = PySimpleGUI.Text("OpenCV Demo", size=(60, 1), justification="center")
+# Need to use copy.copy for object addition to array as list exits if not list type
+my_c.text.append(
+    copy.copy(
+        layout_text
+    )
+)
+my_c.layout_dict.update(layout_text)
+print(my_c.layout_dict)
+exit()
+my_c.image.append(
+    copy.copy(
+        PySimpleGUI.Image(filename="", key="-IMAGE-")
+    )
+)
+my_c.thres_slider.append(
+    # copy.copy(
+        layout.add_slider(
+            radio_name="threshold",
+            radio_key="-THRESH-",
+            slider_key="-THRESH SLIDER-"
+        )
+    # )
+)
+my_c.canny_slider.append(
+    copy.copy(
+        layout.add_slider(
+            radio_name="canny",
+            radio_key="-CANNY-",
+            slider_key="-CANNY-",
+            slider_size=(20, 15)
+        )
+    )
+)
+my_c.blur_slider.append(
+    copy.copy(
+        layout.add_slider(
+            radio_name="blur",
+            radio_key="-BLUR-",
+            slider_key="-BLUR SLIDER-",
+            slider_range=(1, 11),
+            slider_default=1
+        )
+    )
+)
+my_c.hue_slider.append(
+    copy.copy(
+        layout.add_slider(
+            radio_name="hue",
+            radio_key="-HUE-",
+            slider_default=0,
+            slider_key="-HUE SLIDER-")
+    )
+)
+my_c.enhance_slider.append(
+    copy.copy(
+        layout.add_slider(
+            radio_name="enhance",
+            radio_key="-ENHANCE-",
+            slider_key="-ENHANCE SLIDER"
+        )
+    )
+)
+my_c.exit_button.append(
+    copy.copy(
+        PySimpleGUI.Button("Exit", size=(10, 1))
+    )
+)
+
+for i in my_c:
+    print(i)
+exit()
+
+window_layout = list
+# TODO for i in my_c items, append to list, then you have layout
 print(my_c.text)
 
 
@@ -57,22 +136,14 @@ with open("layout.json", "w") as layout_json, open("layout.py", "r") as layout_p
 exit()
 
 sg.theme("LightGreen")
-
 # Create the window and show it without the plot
-window = sg.Window("OpenCV Integration", main2layout, location=(800, 400))
-
+window = sg.Window("OpenCV Integration", window_layout, location=(800, 400))
 cap = cv2.VideoCapture('videos/dyna_pre.mp4')  # Has options, read, isOpened, release
 # print(cap)  # prints memory object reference
-# exit()
-
+# Instead of true, use isOpened for video, and potential repeat if needed
 if not cap.isOpened():
     print("Video not located, please correct.")
-
-# Instead of true, use isOpened for video, and potential repeat if needed
-
 # TODO: open one frame, then with button continue to video
-
-
 exit()
 
 while True:
